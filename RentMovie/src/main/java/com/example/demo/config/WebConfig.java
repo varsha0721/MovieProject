a76fb5f20f979b552223c637956ea9a497324962
ranosysHemant <hemant_kumar@sal.org.sg>
2020-05-16 16:12:30 +0530

    swagger configuration

---
+++ RentMovie/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
